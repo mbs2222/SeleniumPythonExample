14ee4e69e7b6fad814b0fd8292cbef4f15490123
mbs2222 <medbensassi@gmail.com>
2020-11-07 15:40:57 +0100

    Adding Welcome

--- OrangeDemoProject/Tests/Login.py
+++ OrangeDemoProject/Tests/Login.py
@@ -19,5 +19,7 @@ def test_login(test_setup):
     driver.find_element_by_id("txtUsername").send_keys("Admin")
     driver.find_element_by_id("txtPassword").send_keys("admin123")
     driver.find_element_by_id("btnLogin").click()
+    driver.find_element_by_id("welcome").click()
+
 
 
